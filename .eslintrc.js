a4a159be34cbf13b3e649631ee276ac2d7aa05a9
reoreo-zyt <768119359@qq.com>
2024-06-11 01:52:29 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended", // 使用推荐的React规则
+    "plugin:react-hooks/recommended", // 使用推荐的React Hooks规则
+    "plugin:jsx-a11y/recommended", // 使用推荐的a11y规则
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y"],
+  rules: {
+    // 在这里添加或覆盖规则
+  },
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+## 1、react 项目环境搭建
+
+- [package.json](https://docs.npmjs.com/cli/v10/configuring-npm/package-json)
+- [LICENSE](https://choosealicense.com/)
+- .gitignore 使用 vscode 插件
+- 代码规范 EditorConfig 和 [Prettier](https://prettier.io/playground/)
+- `TODO`: eslint styleLint
